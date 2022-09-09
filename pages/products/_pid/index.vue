be1a88d191dbc594e2d40f316f6f7791ce056781
josecis0216 <josecis0216@gmail.com>
2022-09-09 07:20:03 -0600

    initial commit

---
+++ pages/products/_pid/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <section>
+        <h1>product you've chosen {{ $route.params.pid }}</h1>
+        <nuxt-link to="/" class="button--grey" >Home</nuxt-link>
+    </section>
+</template>
+
+<script>
+    export default {
+        validate (data) {
+            console.log(data);
+            return true 
+        }
+    }
+</script>
---
+++ pages/products/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <section>
+        <button @click="loadProductPage(1)">Product 1</button>
+        <button @click="loadProductPage(2)">Product2</button>
+    </section>
+</template>
+
+<script>
+    export default {
+        methods: {
+            loadProductPage(pid) {
+                this.$router.push('/products/' + pid);
+            }
+        }
+    }
+</script>
